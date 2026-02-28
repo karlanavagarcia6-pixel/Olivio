f07990cecaea396967be62f6abd96c406079ec27
karlanavagarcia6-pixel <karlanavagarcia6@gmail.com>
2026-02-27 18:13:23 -0600

    Add files via upload

---
+++ js/partials.js
@@ -0,0 +1,22 @@
+const btnToggleResponsive = document.querySelector('.btn-toggle');
+const menuResponsive = document.querySelector('.menu-responsive');
+const header = document.querySelector('header');
+
+btnToggleResponsive.addEventListener('click', () => {
+
+    const iconBars = document.querySelector('.fa-bars');
+    const iconClose = document.querySelector('.fa-xmark');
+
+    if (iconBars.classList.contains('active')) {
+        iconBars.classList.remove('active');
+        iconClose.classList.add('active');
+        menuResponsive.classList.add('show');
+        menuResponsive.style.top = `${header.clientHeight}px`;
+    } 
+    else {
+        iconBars.classList.add('active');
+        iconClose.classList.remove('active');
+        menuResponsive.classList.remove('show');
+    }
+
+});
